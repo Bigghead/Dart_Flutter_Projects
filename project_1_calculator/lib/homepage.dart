fcc5bb9be44619750fc25efb8fe08b891861f9ab
Bigghead <shavenchew@yahoo.com>
2018-07-18 18:18:00 -0700

    boilerplate + getting homepage setup

---
+++ project_1_calculator/lib/homepage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+
+  @override
+    State<StatefulWidget> createState() {
+      // TODO: implement createState
+      return null;
+    }
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int totalValue = 0;
+  String currentCal = '';
+
+  @override
+    void initState() {
+      // TODO: implement initState
+      super.initState();
+    }
+}
